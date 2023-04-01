37f4e6d52244fee02938372cc4684e359011401b
PaulaStrazdina <124048446+PaulaStrazdina@users.noreply.github.com>
2023-04-01 19:18:22 +0300

    Update programmesanas_projekts.py
    Pievienoju vēlvienu datumu un notikumu ar visu aprakstu, lai pārliecinātos, ka programma pareizi strādā

--- programmesanas_projekts.py
+++ programmesanas_projekts.py
@@ -1,6 +1,6 @@
-dat=['19.06.1919']
-notik=["Cēsu kaujas"]
-notikaprak=['Cēsu kaujas bija viens no nozīmīgākajiem pavērsiena punktiem Latvijas Neatkarības kara gaitā. Kaujas noslēdzās ar Igaunijas armijas un Ziemeļlatvijas brigādes pārliecinošu uzvaru.']
+dat=['19.06.1919','18.11.1918']
+notik=["Cēsu kaujas",'Latvijas Republikas proklamēšanas diena']
+notikaprak=['Cēsu kaujas bija viens no nozīmīgākajiem pavērsiena punktiem Latvijas Neatkarības kara gaitā. Kaujas noslēdzās ar Igaunijas armijas un Ziemeļlatvijas brigādes pārliecinošu uzvaru.','Latvijas Tautas padomes (LTP) formāls solis 1918. gada 18. novembrī, ar kuru suverēnā valsts vara Latvijā pārgāja LTP rokās un tika dibināta neatkarīga valsts']
 a=(input("Ievadi datumu(dd.mm.gggg) vai notikumu(piem. Cēsu kaujas) par kuru vēlies uzzināt! - "))
 if a in dat:
     print(notik[dat.index(a)])
